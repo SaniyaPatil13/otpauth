56cf4f8a23920dfeea698272c27a1eacf390f4f9
SaniyaPatil13 <155056076+SaniyaPatil13@users.noreply.github.com>
2024-10-31 16:54:02 +0530

    Add files via upload

---
+++ lib/otpscreen.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class OTPScreen extends StatefulWidget {
+  String verificationid;
+  OTPScreen({super.key, required this.verificationid});
+
+  @override
+  State<OTPScreen> createState() => _OTPScreenState();
+}
+
+class _OTPScreenState extends State<OTPScreen> {
+  TextEditingController otpController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OTP Screen"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: TextField(
+              controller: otpController,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                  hintText: "Enter The OTP",
+                  suffixIcon: Icon(Icons.phone),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25))),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                try {
+                  PhoneAuthCredential credential =
+                      await PhoneAuthProvider.credential(
+                          verificationId: widget.verificationid,
+                          smsCode: otpController.text.toString());
+                  FirebaseAuth.instance.signInWithCredential(credential).then((value){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHomePage()));
+                  });
+                } catch (ex) {
+                  log(ex.toString() as num);
+                }
+              },
+              child: Text("OTP"))
+        ],
+      ),
+    );
+  }
+}
